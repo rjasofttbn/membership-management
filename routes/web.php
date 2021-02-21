62f147a935e032756c4005582d49bcf65aab4f24
Your Name <you@example.com>
2021-02-21 10:51:01 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\welcomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Route::get('/test2', function () {
+    return view('testing');
+});
+
+Route::post('/test-post-store', function () {
+    //return 'testing for';
+    return request()->all();    
+})->name('test-post');
+
+Route::put('/test-post-update/{student_id}', function () {
+    //return 'testing for';
+    return request()->all();    
+})->name('test-post-update');
+
+Route::delete('/test-post-delete/{student_id}', function () {
+    //return 'testing for';
+   // return 'deleted'; 
+    return redirect()->route('index');   
+})->name('test-post-delete');
+
+Route::get('/', [welcomeController::class, 'index'])->name('index');
+
+Route::get('posts', [PagesController::class, 'posts'])->name('posts');
+Route::get('/posts-view', [PagesController::class, 'postsView'])->name('posts');
